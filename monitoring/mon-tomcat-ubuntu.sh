166521b8051bbbac743d4ac854b27f321ff0724b
kwpark <kw_park@atecmobility.com>
2023-10-24 10:12:42 +0900

    changed tomcat pid counting

--- monitoring/mon-tomcat-ubuntu.sh
+++ monitoring/mon-tomcat-ubuntu.sh
@@ -14,9 +14,13 @@ printf "##### CHECK %s STATUS ##### \n\n" "$SERVICE_NAME"
 # echo "test2 : $(pgrep -f tomcat -l)"
 
 # SERVICE_PID="$(pgrep -f java)"
-SERVICE_PID="$(pgrep -fl java | grep -v mon-tomcat | grep -v grep | awk '{print $1}')"
+# SERVICE_PID="$(pgrep -fl java | grep -v mon-tomcat | grep -v grep | awk '{print $1}')"
+SERVICE_PID="$(ps -ef | grep tomcat | grep apache | grep -v grep | awk '{print $2}')"
 ARR_PID=(${SERVICE_PID}) # split by space
+# PID_CNT="$(ps -ef | grep tomcat | grep apache | grep -v grep | wc -l)"
 PID_CNT=${#ARR_PID[@]}
+
+printf "ARR_PID (%s)(cnt: %d) \n" "${ARR_PID[*]}" "$PID_CNT"
 
 if [ "$PID_CNT" -ne 0 ]; then
     printf "Service %s, status: active (%s) \n" "$SERVICE_NAME" "$(date)"
@@ -31,29 +35,18 @@ if [ "$PID_CNT" -ne 1 ]; then
         printf "Service %s is checking...\n" "$SERVICE_NAME"
         for pid in "${ARR_PID[@]}"; do
             printf "Tomcat process PID %s \n" "$pid"
+            kill -9 "$pid"
+            sleep 3
         done
-        # printf "Service %s is killing...\n" "$SERVICE_NAME"
-        # for pid in "${ARR_PID[@]}"; do
-        #     printf "Kill -2 %s \n" "$pid"
-        #     kill -2 "$pid"
-        #     sleep 1
-        # done
-
-        # sleep 1
     else
         printf "Service %s is nothing...\n" "$SERVICE_NAME"
     fi
 
-    printf "Service %s is stopping...\n" "$SERVICE_NAME"
-    printf "Service path: %s/shutdown.sh \n" "$SERVICE_PATH"
-    # shellcheck source=/dev/null
-    . $SERVICE_PATH/shutdown.sh
-    sleep 3
     printf "Service %s is restarting...\n" "$SERVICE_NAME"
     printf "Service path: %s/startup.sh \n" "$SERVICE_PATH"
     # shellcheck source=/dev/null
-    . $SERVICE_PATH/startup.sh
-    sleep 5
+    sh $SERVICE_PATH/startup.sh
+    sleep 10
 else
     printf "Service %s(PID: %s) is already running...\n" "$SERVICE_NAME" "$SERVICE_PID"
 fi
