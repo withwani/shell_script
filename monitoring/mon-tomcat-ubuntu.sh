22229c8bbcb040edbd986f3daf2e50e32f616525
kwpark <kw_park@atecmobility.com>
2023-10-24 19:18:45 +0900

    changed to using CATALINA_PID

--- monitoring/mon-tomcat-ubuntu.sh
+++ monitoring/mon-tomcat-ubuntu.sh
@@ -13,24 +13,24 @@ printf "##### CHECK %s STATUS ##### \n\n" "$SERVICE_NAME"
 # echo "test1 : $(pgrep -f java -l)"
 # echo "test2 : $(pgrep -f tomcat -l)"
 
-# SERVICE_PID="$(pgrep -f java)"
-# SERVICE_PID="$(pgrep -fl java | grep -v mon-tomcat | grep -v grep | awk '{print $1}')"
-SERVICE_PID="$(ps -ef | grep tomcat | grep apache | grep -v grep | awk '{print $2}')"
-ARR_PID=(${SERVICE_PID}) # split by space
-# PID_CNT="$(ps -ef | grep tomcat | grep apache | grep -v grep | wc -l)"
-PID_CNT=${#ARR_PID[@]}
+PID_FILE="$SERVICE_PATH/catalina.pid"
 
-printf "ARR_PID (%s)(cnt: %d) \n" "${ARR_PID[*]}" "$PID_CNT"
+if [ -r "$PID_FILE" ]; then
+    SERVICE_PID="$(cat $PID_FILE)"
+    printf "SERVICE_PID (%s) \n" "$SERVICE_PID"
 
-if [ "$PID_CNT" -ne 0 ]; then
     printf "Service %s, status: active (%s) \n" "$SERVICE_NAME" "$(date)"
+
+    printf "Service %s(PID: %s) is already running...\n" "$SERVICE_NAME" "$SERVICE_PID"
 else
     printf "Service %s, status: inactive (%s) \n" "$SERVICE_NAME" "$(date)"
-fi
 
-printf "Service %s, PID cnt: %d \n" "$SERVICE_NAME" "$PID_CNT"
+    SERVICE_PID="$(ps -ef | grep tomcat | grep apache | grep -v grep | awk '{print $2}')"
+    ARR_PID=(${SERVICE_PID})
+    PID_CNT=${#ARR_PID[@]}
 
-if [ "$PID_CNT" -ne 1 ]; then
+    printf "ARR_PID (%s)(cnt: %d) \n" "${ARR_PID[*]}" "$PID_CNT"
+
     if [ "$PID_CNT" -ne 0 ]; then
         printf "Service %s is checking...\n" "$SERVICE_NAME"
         for pid in "${ARR_PID[@]}"; do
@@ -45,10 +45,8 @@ if [ "$PID_CNT" -ne 1 ]; then
     printf "Service %s is restarting...\n" "$SERVICE_NAME"
     printf "Service path: %s/startup.sh \n" "$SERVICE_PATH"
     # shellcheck source=/dev/null
-    sh $SERVICE_PATH/startup.sh
-    sleep 10
-else
-    printf "Service %s(PID: %s) is already running...\n" "$SERVICE_NAME" "$SERVICE_PID"
+    # sh $SERVICE_PATH/startup.sh
+    # sleep 10
 fi
 
 printf "=======================================================================================\n"
